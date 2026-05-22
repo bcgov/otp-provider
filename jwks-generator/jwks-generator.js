49a833582dd245301c2c293d7a79dcf26efe491f
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2026-05-21 22:33:50 -0700

    fix: generate a jwks on the fly for playwright tests

--- jwks-generator/jwks-generator.js
+++ jwks-generator/jwks-generator.js
@@ -8,7 +8,7 @@ async function createKeystore() {
 
   // Export the public key set (pass 'false' to exclude private keys)
   const jwks = keystore.toJSON(true);
-  console.log(JSON.stringify(jwks, null, 2).replace(/[\r\n]+/g, ' '));
+  console.log(JSON.stringify(jwks).replace(/[\r\n]+/g, ' '));
 }
 
 createKeystore();
